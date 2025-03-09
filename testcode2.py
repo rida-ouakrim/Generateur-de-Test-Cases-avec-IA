d3bd5a5953458df33973fde2d01c645542679f62
OUAKRIM Rida <ridaouakrim0@gmail.com>
2025-03-09 23:05:50 +0000

    Update testcode2.py

--- testcode2.py
+++ testcode2.py
@@ -701,7 +701,7 @@ with col_input:
     
     # Section Exigence
     with st.expander("Exigence", expanded=True):
-        req_tab1 = st.tabs(["Saisie manuelle"])
+        req_tab1 ,req_tab2 = st.tabs(["Saisie manuelle", "Upload de fichier"])
         
         with req_tab1:
             requirements_input = st.text_area("Saisissez votre exigence", height=150)
